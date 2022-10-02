b4e815eab53d4db2d147dc92e6f87e5813bf25a4
jochongs <joch2712@naver.com>
2022-10-02 16:30:51 +0000

    Do refactoring login page layout

--- public/js/login.js
+++ public/js/login.js
@@ -5,28 +5,23 @@ const loginEvent = async ()=>{
     
     //에러 메시지 초기화
     errorDiv.innerHTML = "";
-    try{
-        const response = await fetch('/session',{
-            "method" : "POST",
-            "headers" : {
-                "Content-Type" : "application/json"
-            },
-            "body" : JSON.stringify({
-                "id" : idValue,
-                "pw" : pwValue,
-            })
+    const response = await fetch('/session',{
+        "method" : "POST",
+        "headers" : {
+            "Content-Type" : "application/json"
+        },
+        "body" : JSON.stringify({
+            "id" : idValue,
+            "pw" : pwValue,
         })
-        const result = await response.json();
+    })
+    const result = await response.json();
 
-        if(result.state){ //로그인 성공시
-            location.href = '/'; 
-        }else if(result.error.DB){ //DB에러 발생시
-            throw result.error.errorMessage;
-        }else{
-            errorDiv.innerHTML = result.error.errorMessage;
-        }
-    }
-    catch{
-        location.href = "/error";
+    if(result.state){ //로그인 성공시
+        location.href = '/'; 
+    }else if(result.error.DB){ //DB에러 발생시
+        location.href = '/page/error';
+    }else{
+        errorDiv.innerHTML = `*${result.error.errorMessage}`;
     }
 }
