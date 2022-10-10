194546a5cae2466403f82612f152cbf6213553a4
jochongs <joch2712@naver.com>
2022-10-10 17:34:17 +0000

    git error fix try

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 const path = require('path');
 const dotenv = require('dotenv');
 const session = require('express-session');
+const requestIp = require('request-ip');
+const geoip = require('geoip-lite');
+const fs = require('fs');
+const https = require('https');
 
 const sessionApi = require('./router/session');
 const pagesApi = require('./router/pages');
@@ -11,11 +15,16 @@ const accountApi = require('./router/account');
 const postApi = require('./router/post');
 const commentApi = require('./router/comment');
 const authApi = require('./router/auth');
-
+//const accountNOSQLAPI = require('./router/accountNoSQL');
 
 //설정 =========================================================================================================================================================
 dotenv.config();
 const PUBLIC_PATH = path.join(__dirname,'public');
+const options = {
+    "key" : fs.readFileSync('/etc/letsencrypt/live/backend.xn--289a320aihm.com/privkey.pem'),
+    "cert" : fs.readFileSync('/etc/letsencrypt/live/backend.xn--289a320aihm.com/cert.pem'),
+    "ca" : fs.readFileSync('/etc/letsencrypt/live/backend.xn--289a320aihm.com/fullchain.pem')
+}
 
 //전역 미들웨어 =====================================================================================================================================================
 app.use(express.json());
@@ -25,29 +34,47 @@ app.use(session({
     resave : false,
     saveUninitialized : true,
 }))
+// app.use('/',(req,res,next)=>{
+//     //const ip = requestIp.getClientIp(req);
+//     //console.log(geoip.lookup(ip));
+//     next();
+// })
 app.use("/page",pagesApi); 
 app.use('/account',accountApi);
 app.use('/session',sessionApi);
 app.use('/post',postApi);
 app.use('/comment',commentApi);
 app.use('/auth',authApi);
+//app.use('/nosql',accountNOSQLAPI);
+
 
 //페이지==========================================================================================================================================================
+//https로 리다이렉트
+app.get('*',(req,res,next)=>{
+    const protocol = req.protocol;
+    if(protocol === 'https'){
+        next();
+    }else{
+        const des = "https://" + req.hostname + ":443" + req.url;
+        console.log('리다이렉트 됨');
+        res.redirect(des);
+    }
+})
+
 //메인페이지
 app.get('/',(req,res)=>{
     res.sendFile(path.join(PUBLIC_PATH,'html','index.html'));
 });
 
-//404예외처리
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(PUBLIC_PATH,'html','error404.html'));
+    res.sendFile((path.join(PUBLIC_PATH,'html','error404.html')));
 })
 
-
 //listen
-app.listen(process.env.PORT,()=>{
-    console.log(`web server on  PORT : ${process.env.PORT}`); //https://nodejs.org/api/process.html#process_process_env
+app.listen(process.env.PORT,'0.0.0.0',()=>{
+    console.log(`web server on  PORT : ${process.env.PORT}`); 
 });
 
-
-//cohesion이 함수형 프로그래밍의 트렌드 -> 객체는 객체지향 설계를 하면서 유기적으로 연결되면서 설계를 함. 함수안에서 다 해결되도록 함수형으로 코딩하는 것을 말함
+https.createServer(options,app).listen(process.env.HTTPS_PORT,'0.0.0.0',()=>{
+    console.log(`web server on PORT : ${process.env.HTTPS_PORT}`)
+})
